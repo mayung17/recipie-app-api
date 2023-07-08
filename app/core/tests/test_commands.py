e9351d1f0a86e44ccdbd19a36b0c2fed680dca3b
mayung <mayungrai17@gmail.com>
2023-07-08 17:37:13 +0545

    COnfigured docker compose and checks to wait for db

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,5 +1,5 @@
 """
-Test custom Django management commmands.
+Test custom Django management commands.
 """
 
 from unittest.mock import patch
@@ -17,20 +17,19 @@ class CommandTests(SimpleTestCase):
 
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready."""
-        patched_check.return_value =True
+        patched_check.return_value = True
 
         call_command('wait_for_db')
 
         patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
-    def test_wait_for_db_delay(self, patched_sleep,patched_check):
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting Operationalerror"""
-
         patched_check.side_effect = [Psycopg2Error] * 2 + \
             [OperationalError] * 3 + [True]
-        
+
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.asset_called_with(databases=['default'])
+        patched_check.assert_called_with(databases=['default'])
