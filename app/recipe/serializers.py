ab07fd665869b8db6d8f8664eecf667c8304427d
mayung <mayungrai17@gmail.com>
2023-07-18 14:43:20 +0545

    Added destroy endpoint

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -2,7 +2,7 @@
 
 from rest_framework import serializers
 
-from core.models import Recipe
+from core.models import (Recipe,Tag)
 
 
 class RecipeSerializer(serializers.ModelSerializer):
@@ -18,6 +18,14 @@ class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for recipe detail view"""
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """serializer for tags"""
+    class Meta:
+        model=Tag
+        fields = ['id','name']
+        read_only_fields = ['id',]
         
 
         
